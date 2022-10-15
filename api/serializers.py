b92c752bbf76e6b7cd167f492e328b60c46bd1e7
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-15 06:12:43 +0200

    No.Of.Rating, Avg.Of.Rating Added to Meal Model

--- api/serializers.py
+++ api/serializers.py
@@ -7,7 +7,7 @@ from .models import Meal, Rating
 class MealSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meal
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'noOfRatings', 'avgRatings')
 
 
 class RatingSerializer(serializers.ModelSerializer):
