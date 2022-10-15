7df7814a9ff32a8916602d55df69aa5402bb6ffb
iNightjar <mohamedmedhateltlawy@gmail.com>
2022-10-16 00:21:36 +0200

    UserViewSet router url

--- api/urls.py
+++ api/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from rest_framework import routers
 from django.conf.urls import include
-from .views import MealViewSet, RatingViewSet
+from .views import MealViewSet, RatingViewSet, UserViewSet
 
 
 # adding routers
 router = routers.DefaultRouter()
 router.register('meals', MealViewSet)
 router.register('ratings', RatingViewSet)
+router.register('users', UserViewSet)
+
 
 
 urlpatterns = [
